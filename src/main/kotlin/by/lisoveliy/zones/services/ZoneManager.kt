c778d5ec053680fda4de23fb41760f6098b9089d
Lisoveliy <1986developer@gmail.com>
2024-07-04 17:06:32 +0300

    Added sounds

--- src/main/kotlin/by/lisoveliy/zones/services/ZoneManager.kt
+++ src/main/kotlin/by/lisoveliy/zones/services/ZoneManager.kt
@@ -2,10 +2,16 @@ package by.lisoveliy.zones.services
 
 import by.lisoveliy.zones.extensions.PositionExtensions.getIntegerVector
 import by.lisoveliy.zones.models.Zone
+import net.minecraft.core.Holder
 import net.minecraft.core.Vec3i
 import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.Style
+import net.minecraft.network.protocol.game.ClientboundSetSubtitleTextPacket
 import net.minecraft.network.protocol.game.ClientboundSetTitleTextPacket
+import net.minecraft.network.protocol.game.ClientboundSoundPacket
 import net.minecraft.server.level.ServerPlayer
+import net.minecraft.sounds.SoundEvents
+import net.minecraft.sounds.SoundSource
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.levelgen.structure.BoundingBox
@@ -21,7 +27,7 @@ class ZoneManager {
 
     init {
         val box = BoundingBox(-3, -3, -3, 3, 3, 3)
-        zones.add(Zone(box, "Test zone"))
+        zones.add(Zone(box, "Деревня"))
     }
 
     private fun getZonesFromPosition(playerPos: Vec3i): List<Zone> {
@@ -39,8 +45,8 @@ class ZoneManager {
         blockTick = true
 
         val players = world.players()
-        playerZones.forEach{ existingPlayer ->
-            if(!players.any { x -> x.uuid == existingPlayer.key } && players.isNotEmpty()){
+        playerZones.forEach { existingPlayer ->
+            if (!players.any { x -> x.uuid == existingPlayer.key } && players.isNotEmpty()) {
                 playerZones.remove(existingPlayer.key)
             }
         }
@@ -74,11 +80,38 @@ class ZoneManager {
     private fun updateZones(enteredZones: List<Zone>, exitedZones: List<Zone>, player: Player) {
         enteredZones.forEach { zone: Zone ->
             playerZones[player.uuid]!!.add(zone)
-            (player as ServerPlayer).connection.send(ClientboundSetTitleTextPacket(Component.literal("${zone.name} entered")))
+            (player as ServerPlayer).connection.send(
+                ClientboundSoundPacket(
+                    Holder.direct(SoundEvents.EXPERIENCE_ORB_PICKUP),
+                    SoundSource.WEATHER,
+                    player.position().x,
+                    player.position().y,
+                    player.position().z,
+                    1f,
+                    1f,
+                    1
+                )
+            )
+            player.connection.send(ClientboundSetTitleTextPacket(Component.literal("§a${zone.name}")))
+            player.connection.send(ClientboundSetSubtitleTextPacket(Component.literal("§6Вы в зоне")))
         }
         exitedZones.forEach { zone: Zone ->
             playerZones[player.uuid]!!.remove(zone)
-            (player as ServerPlayer).connection.send(ClientboundSetTitleTextPacket(Component.literal("${zone.name} exited")))
+            (player as ServerPlayer).connection.send(
+                ClientboundSoundPacket(
+                    Holder.direct(SoundEvents.EXPERIENCE_ORB_PICKUP),
+                    SoundSource.WEATHER,
+                    player.position().x,
+                    player.position().y,
+                    player.position().z,
+                    1f,
+                    1f,
+                    1
+                )
+            )
+            player.playSound(SoundEvents.EXPERIENCE_BOTTLE_THROW, 1f, 1f)
+            player.connection.send(ClientboundSetTitleTextPacket(Component.literal("§5${zone.name}")))
+            player.connection.send(ClientboundSetSubtitleTextPacket(Component.literal("§4Вы покинули зону")))
         }
     }
 }
